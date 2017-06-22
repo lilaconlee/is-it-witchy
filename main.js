d24f7990b04461f1c887ba03b59d7836ca40e2cb
lilaconlee <lila.conlee@gmail.com>
2017-06-21 19:50:17 -0500

    Test with adjectives

--- main.js
+++ main.js
@@ -13,13 +13,21 @@ form.addEventListener('submit', witchCheck);
 function witchCheck(event) {
   event.preventDefault();
 
-  if (isWitchy(input.value)) {
-    result.textContent = 'witchy';
-  } else {
-    result.textContent = 'not witchy';
-  }
+  result.textContent = isWitchy(input.value) ? 'witchy' : 'not witchy';
 }
 
-function isWitchy(value) {
-  return items.indexOf(value.toLowerCase()) !== -1 || pluralItems.indexOf(value.toLowerCase()) !== -1
+function isWitchy(it) {
+  return contains(items, it) || contains(pluralItems, it) || testSubstrings(it);
+}
+
+function testSubstrings(it) {
+  return items.some(function(item) {
+    var re = new RegExp(item, 'g');
+    return re.test(it);
+  });
+}
+
+function contains(array, value) {
+  var result = array.indexOf(value.toLowerCase()) !== -1;
+  return result;
 }
